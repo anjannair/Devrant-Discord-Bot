836cfcfaf2fc2ed9d46633362eb3f156355366fc
Anjan Nair <22571613+anjannair@users.noreply.github.com>
2020-11-04 17:47:38 +0530

    Added TL;DR for links

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { CommandoClient } = require('discord.js-commando');
 const Discord = require("discord.js");
+const fetch = require('node-fetch');
 const path = require('path');
 require('dotenv').config();
 const config = require( path.resolve( __dirname, "config.json" ) );
@@ -29,6 +30,32 @@ client.once('ready', () => {
 	console.log(`Logged in as ${client.user.username}`);
 	client.user.setActivity(config.activity.game, {type:'STREAMING'});
 });
+
+client.on('message', async message =>{
+	function isValidURL(string) {
+		//regex for link
+		const res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+		return (res !== null);
+	}
+	if(isValidURL(message.content.toLowerCase())==true){
+		let storyfetcher = `https://api.smmry.com/&SM_API_KEY=${process.env.TLDR}&SM_URL=${message.content.toLowerCase()}`
+        let response = await fetch(storyfetcher).catch(err =>{
+			console.log("error");
+		});
+		let data = await response.json();
+		if(!data) return;
+		if(data.sm_api_error) return;
+		if(data.sm_api_character_count > 2048) return;
+		
+		const embed = new Discord.MessageEmbed()
+			.setTitle(data.sm_api_title)
+			.setColor('#FF7F50')
+			.setDescription(data.sm_api_content)
+			.setFooter(`I have reduced the article for you by ${data.sm_api_content_reduced}`)
+		message.channel.send(embed);
+	}
+	else return;
+})
 
 client.on('error', console.error);
 
