e69481dd530b58d20155f670bbfc2ba249d2a869
Anjan Nair <22571613+anjannair@users.noreply.github.com>
2020-11-19 10:33:28 +0530

    [Added] Change nickname and pro tips

--- commands/tools/dev-verify.js
+++ commands/tools/dev-verify.js
@@ -5,30 +5,30 @@ const apiurl = require("./rantapi.js")
 
 
 module.exports = class verify extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'verify',
-			group: 'tools',
-			memberName: 'verify',
+    constructor(client) {
+        super(client, {
+            name: 'verify',
+            group: 'tools',
+            memberName: 'verify',
             description: 'Verify yourself on devRant and get the verified role',
             guildOnly: true,
             throttling: {
                 usages: 1,
                 duration: 30,
             },
-            clientPermissions: ['MANAGE_ROLES','EMBED_LINKS']
-		});
-	}
+            clientPermissions: ['MANAGE_ROLES', 'EMBED_LINKS']
+        });
+    }
 
-	async run(message) {
-        
+    async run(message) {
+
         //checking if already verified
         let alreadyverified = message.member.roles.cache.find(role => role.id === process.env.VERIFIED)
         const alreadyverifembed = new Discord.MessageEmbed()
             .setColor('#00FF00')
-            .setDescription("__You are already verified!__\n\n")
-            .setFooter("Enjoy","https://emoji.gg/assets/emoji/9192_random_tick.png");
-        if(alreadyverified) return message.reply(alreadyverifembed)
+            .setDescription("__You are already verified!__\n\nPro tip: Use `*rant` to see rants from devRant")
+            .setFooter("Enjoy", "https://emoji.gg/assets/emoji/9192_random_tick.png");
+        if (alreadyverified) return message.reply(alreadyverifembed)
 
         //creating captcha
         const captcha = Math.random().toString(36).slice(2, 8);
@@ -40,9 +40,9 @@ module.exports = class verify extends Command {
         let idfetcher = `${apiurl.url}/get-user-id?app=3&username=${message.member.displayName}`;
         let response = await fetch(idfetcher);
         let data = await response.json();
-        
+
         //checking for invalid username
-        if(!data.user_id) return message.reply("__Your username or nickname on Discord does not match the one on devRant!__")
+        if (!data.user_id) return message.reply("__Your username or nickname on Discord does not match the one on devRant!__\n\nPro tip: To change your nickname just use the `*nick` command")
         userid = data.user_id;
 
         const embed = new Discord.MessageEmbed()
@@ -50,24 +50,24 @@ module.exports = class verify extends Command {
             .setDescription(`-connect+discord+${captcha}-`);
         message.author.send(embed);    //sends the captcha
 
-        try{
-        const msg = await message.reply("Sent you the verification token\n\nNow head to devRant and comment the token in the format sent (\`-connect+discord+TOKEN-\`) on the main post\n\nLink: https://devrant.com/collabs/3221539/devrant-community-server-in-discord\n\n**ENSURE IT'S YOUR RECENT COMMENT\nAFTER YOU FINISH COMMENTING THE TOKEN TYPE \`DONE\` HERE**");
+        try {
+            const msg = await message.reply("Sent you the verification token\n\nNow head to devRant and comment the token in the format sent (\`-connect+discord+TOKEN-\`) on the main post\n\nLink: https://devrant.com/collabs/3221539/devrant-community-server-in-discord\n\n**ENSURE IT'S YOUR RECENT COMMENT\nAFTER YOU FINISH COMMENTING THE TOKEN TYPE \`DONE\` HERE**");
 
-        try{
-            const filter = m => {
-                if(m.author.bot) return;
-                if(m.author.id === message.member.id && m.content.toLowerCase() === "done") return true;
-                if(m.content.toLowerCase() != "done" && m.author.id === message.member.id){
-                    m.say('Please type \`DONE\`');
-                    return false;
-                }
-                else{
-                    return false;
-                }
-            };
+            try {
+                const filter = m => {
+                    if (m.author.bot) return;
+                    if (m.author.id === message.member.id && m.content.toLowerCase() === "done") return true;
+                    if (m.content.toLowerCase() != "done" && m.author.id === message.member.id) {
+                        m.say('Please type \`DONE\`');
+                        return false;
+                    }
+                    else {
+                        return false;
+                    }
+                };
 
-            const response = await msg.channel.awaitMessages(filter, { max: 1, time: 600000, errors: ['time']});
-                if(response){
+                const response = await msg.channel.awaitMessages(filter, { max: 1, time: 600000, errors: ['time'] });
+                if (response) {
                     //getting the latest comment by ID
                     let commentfetcher = `${apiurl.url}/users/${userid}?app=3`
                     let response2 = await fetch(commentfetcher);
@@ -84,32 +84,31 @@ module.exports = class verify extends Command {
                         .setColor("#FF0000")
                         .setDescription("Couldn't find the token\nEnsure your recent comment was the token")
 
-                    if(rantcommentid != process.env.MAINRANT) return message.author.send(notfoundrantcommentid);
-                    if(rantbody != `-connect+discord+${captcha}-`) return message.author.send(notfoundrantbody);
+                    if (rantcommentid != process.env.MAINRANT) return message.author.send(notfoundrantcommentid);
+                    if (rantbody != `-connect+discord+${captcha}-`) return message.author.send(notfoundrantbody);
 
                     //finding the role
                     let verified = message.member.guild.roles.cache.find(role => role.id === process.env.VERIFIED);
                     let unverified = message.member.guild.roles.cache.find(role => role.id === process.env.UNVERIFIED);
-                    if(verified){ 
-                        message.member.roles.add(verified).catch(err =>
-                            {
-                                return message.reply("I do not have permissions to add the role to you😭")
-                            });
+                    if (verified) {
+                        message.member.roles.add(verified).catch(err => {
+                            return message.reply("I do not have permissions to add the role to you😭")
+                        });
                         message.member.roles.remove(unverified);
                         const verifembed = new Discord.MessageEmbed()
                             .setColor('#00FF00')
                             .setDescription("You have been verified\n\n")
-                            .setFooter("Signing off","https://emoji.gg/assets/emoji/9192_random_tick.png");
+                            .setFooter("Signing off", "https://emoji.gg/assets/emoji/9192_random_tick.png");
                         message.author.send(verifembed);
                     }
-                    if(!verified) console.log("No verified tag");
+                    if (!verified) console.log("No verified tag");
                 }
             }
-            catch(err){
+            catch (err) {
                 console.log(err);
             }
         }
-        catch(err){
+        catch (err) {
             console.log(err);
         }
     }
