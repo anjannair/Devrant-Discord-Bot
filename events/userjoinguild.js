0d340241386796c9f4637690eb281eb021308b10
Anjan Nair <22571613+anjannair@users.noreply.github.com>
2021-02-01 12:48:43 +0530

    Fixed emoji errors , improvised DMs

--- events/userjoinguild.js
+++ events/userjoinguild.js
@@ -1,17 +1,18 @@
 const Discord = require("discord.js");
-
+const { Command } = require('discord.js-commando');
 /* No welcome for bots */
 
 module.exports = async (member) => {
+    const fire = this.client.emojis.cache.get("764807476696055839");
     if (member.user.bot) return;
     var channel = member.guild.channels.cache.find(ch => ch.id === process.env.WELCOME);
     if (!channel) return;
-    channel.send(`Hello, ${member}! We are glad to see you at dRCS :fireblob:`);
+    channel.send(`Hello, ${member}! We are glad to see you at dRCS ${fire}`);
 
     var embs = new Discord.MessageEmbed()
         .setColor('#FF7F50')
-        .setTitle(`Welcome to ${member.guild.name}`)
-        .setDescription(`You are now with ${member.guild.memberCount} members\n\n**Optionally you can verify your devRant identity on the <#${process.env.SPAM}> channel by using \`*verify\` command!**`);
+        .setTitle(`Welcome to the devRant Community Server (dRCS)!`)
+        .setDescription(`Please familiarize with the <#${process.env.RULES}>\n\n**Optionally you can verify your devRant identity on the <#${process.env.SPAM}> channel by using \`*verify\` command!**\n\nThank you and stay tuned!!`);
     member.send(embs).catch(err => {
         return;
     });
--- events/userleaveguild.js
+++ events/userleaveguild.js
@@ -1,10 +1,12 @@
+const { Command } = require('discord.js-commando');
 module.exports = async member => {
+    const lag = this.client.emojis.cache.get("764808105653043202");
     if (member.user.bot) return;
 
     var channel = member.guild.channels.cache.find(ch => ch.id === process.env.WELCOME);
     if (!channel) return;
 
-    channel.send(`Sob-sob... ${member.user.tag} has left the server :lag:`);
+    channel.send(`Sob-sob... ${member.user.tag} has left the server ${lag}`);
 };
 module.exports.help = {
     event: 'guildMemberRemove'
